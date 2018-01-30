98343f56fbec97bd494dd837c0987e70487f964b
BadboYBG <minhnv0110@gmail.com>
2018-01-31 00:18:46 +0700

    finish layout home

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module ThanhFlower
+  class Application < Rails::Application
+    config.load_defaults 5.1
+	config.generators do |g|
+ 		g.test_framework false
+	end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get "/pages/:page" => "pages#show"
+  root "pages#show", page: "home"
+
+end
